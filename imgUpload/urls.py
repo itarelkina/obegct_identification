404883705f2c43c973ac7eff518d95ccfcdb8f10
Irina Tarelkina <itarelkina@yahoo.ca>
2020-12-09 17:36:12 -0500

    Basic function of object identification

--- imgUpload/urls.py
+++ imgUpload/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from django.urls import path
 from . import views
 urlpatterns = [
-    url('imageprocess', views.imageprocess, name='imageprocess'),
+    url('imageprocess', views.imageProcess, name='imageprocess'),
     url(r'^$',views.home, name='home'),
     #path('', views.home, name='home')
 ]
