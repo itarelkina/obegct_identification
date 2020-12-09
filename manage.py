5c285e07c242bf1bae0b47911058e11b63b197a9
Irina Tarelkina <itarelkina@yahoo.ca>
2020-12-09 16:27:51 -0500

    added Image app

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'obegct_identification.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'object_identification.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
